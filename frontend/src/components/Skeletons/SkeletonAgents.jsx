50d815520b7813a1043193f5a02e9abb8f4e1ca6
LuciLua <luci.lua81@gmail.com>
2022-03-01 12:46:03 -0300

    🦴 Skeletons Add

---
+++ frontend/src/components/Skeletons/SkeletonAgents.jsx
@@ -0,0 +1,27 @@
+// import Shimmer from "./Shimmer";
+import Shimmer from "./Shimmer";
+import SkeletonElement from "./SkeletonElement";
+
+const SkeletonAgents = ({ theme }) => {
+  const themeClass = theme || "light";
+
+  return (
+    <>
+      <div className={`skeleton-wrapper ${themeClass}`}>
+        <div className="skeleton-agents">
+          <SkeletonElement type="cardTitle" />
+
+          <div className="skeleton-wrraperTypeIcon">
+            <SkeletonElement type="cardIcon" />
+            <SkeletonElement type="cardType" />
+          </div>
+
+          <SkeletonElement type="cardImage" />
+        </div>
+        <Shimmer />
+      </div>
+    </>
+  );
+};
+
+export default SkeletonAgents;
--- frontend/src/pages/agents.jsx
+++ frontend/src/pages/agents.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 // Styles
 import styles from "../styles/agents.module.scss";
@@ -15,9 +15,19 @@ import {
 } from "react-icons/gi";
 
 import Head from "next/head";
+import SkeletonAgents from "../components/Skeletons/SkeletonAgents";
 
 function Agents() {
   const listAgents = AgentsAPI();
+
+  const [agents, setAgents] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const list = listAgents;
+      setAgents(list);
+    }, 2000);
+  });
 
   return (
     <>
@@ -26,31 +36,37 @@ function Agents() {
       </Head>
       <main>
         <div className={styles.container}>
-          {listAgents.map((agent) => {
-            return (
-              <div
-                className={styles.agent}
-                key={agent.id}
-                type={`${agent.type}`}
-              >
-                <span className={styles.titleAgent}>{agent.name}</span>
-                <div className={styles.types}>
-                  <span type={`${agent.type}`} className={styles.typeIcon}>
-                    {agent.type == "controlador" ? <GiStarMedal /> : ""}
-                    {agent.type == "duelista" ? <GiCrossedSwords /> : ""}
-                    {agent.type == "iniciador" ? <GiStrikingArrows /> : ""}
-                    {agent.type == "sentinela" ? <GiCherish /> : ""}
-                  </span>{" "}
-                  <span type={`${agent.type}`} className={styles.typeName}>
-                    {agent.type}
-                  </span>
+          {agents &&
+            listAgents.map((agent) => {
+              return (
+                <div
+                  className={styles.agent}
+                  key={agent.id}
+                  type={`${agent.type}`}
+                >
+                  <span className={styles.titleAgent}>{agent.name}</span>
+                  <div className={styles.types}>
+                    <span type={`${agent.type}`} className={styles.typeIcon}>
+                      {agent.type == "controlador" ? <GiStarMedal /> : ""}
+                      {agent.type == "duelista" ? <GiCrossedSwords /> : ""}
+                      {agent.type == "iniciador" ? <GiStrikingArrows /> : ""}
+                      {agent.type == "sentinela" ? <GiCherish /> : ""}
+                    </span>{" "}
+                    <span type={`${agent.type}`} className={styles.typeName}>
+                      {agent.type}
+                    </span>
+                  </div>
+                  <div className={styles.agentImage}>
+                    <Image src={agent.image} layout="fill" />
+                  </div>
                 </div>
-                <div className={styles.agentImage}>
-                  <Image src={agent.image} layout="fill" />
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+
+          {!agents &&
+            [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18].map((n) => (
+                <SkeletonAgents theme="light" key={n} />
+            ))}
         </div>
       </main>
     </>
